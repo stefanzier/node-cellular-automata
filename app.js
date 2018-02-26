f25c0b29c29903af33dbf226b6381474bb65af36
Stefan Zier <szier@icloud.com>
2018-02-26 11:11:05 -0800

    Take user input via prompt.js

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const automata = require('./automata');
+const prompt = require("prompt");
 
-let input = '                    *                    ';
-let patternNumber = 126;
-let generations = 100;
+const automata = require("./automata");
 
-automata.plot('1D', input, patternNumber, generations);
+prompt.start();
+
+prompt.get(["type", "input", "pattern", "generations"], (err, result) => {
+  switch (result.type) {
+    case "1D":
+      automata.plot("1D", result.input, result.pattern, result.generations);
+      break;
+    default:
+      console.log("Invalid pattern. Exiting now...");
+  }
+});
